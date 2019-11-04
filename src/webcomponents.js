2fcec8db1981366d3433a6312472e99ff396a518
Johan Westling <johan@westling.ax>
2019-11-04 10:41:47 +0200

    Added try/catch for polyfills loader

--- src/webcomponents.js
+++ src/webcomponents.js
@@ -1,29 +1,37 @@
 import AppComponents from "./component-loaders";
 
-const MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+const MutationObserver =
+  window.MutationObserver || window.WebKitMutationObserver;
 const componentsLoaded = {};
 
 export default (() => {
-  document.addEventListener('DOMContentLoaded', () => componentsInit());
-  // window.WebComponents.waitFor(() => componentsInit());
+  try {
+    window.WebComponents.waitFor(componentsInit);
+  } catch (e) {
+    console.log(`Failed to load polyfills loader!`);
+    componentsInit();
+  }
 })();
 
 function componentsFinder() {
-  // const components = [...document.querySelectorAll(":not(:defined)")].filter(
-  //   el => el.localName.startsWith(`app-`)
-  // );
-
-  const els = document.querySelectorAll(":not(:defined)") || document.getElementsByTagName("*");
+  const els =
+    document.querySelectorAll(":not(:defined)") ||
+    document.getElementsByTagName("*");
   const components = [...els].filter(el => el.localName.startsWith(`app-`));
 
   for (var i in components) {
     const component = components[i];
 
     if (!(component.localName in componentsLoaded)) {
-      componentsLoaded[component.localName] = AppComponents[component.localName]
-        .then(response => {
-          if (response && typeof response.default) setTimeout(response.default, Math.floor(Math.random()*(2000-1000+1)+1000));
-        });
+      componentsLoaded[component.localName] = AppComponents[
+        component.localName
+      ].then(response => {
+        if (response && typeof response.default)
+          setTimeout(
+            response.default,
+            Math.floor(Math.random() * (2000 - 1000 + 1) + 1000)
+          );
+      });
     }
   }
 }
